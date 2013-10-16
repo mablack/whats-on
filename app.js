574d72b5e49e80d4516926e0406cd9eafb5fc6ab
Michael Black <mblack@odecee.com.au>
2013-10-16 22:26:47 +1100

    added basic express app

---
+++ app.js
@@ -0,0 +1,36 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+// var routes = require('./routes');
+var api = require('./routes/api');
+var http = require('http');
+var path = require('path');
+var hbs = require('hbs');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.engine('hbs', hbs.__express);
+app.set('view engine', 'hbs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/api', api.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
